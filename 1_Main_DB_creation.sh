5e4ff207a01d4f9e209847f5b05c9791071bf5d5
Matthew Luckie <mjl@luckie.org.nz>
2016-02-06 17:10:57 -0800

    add id columns to all tables

--- 1_Main_DB_creation.sh
+++ 1_Main_DB_creation.sh
@@ -4,6 +4,7 @@ mysql -u root -p$PASSWORD -Bse "CREATE DATABASE IF NOT EXISTS ASRank;"
   
   
 mysql -u root -p$PASSWORD -D  ASRank -s -e "CREATE TABLE IF NOT EXISTS ASRank.ASRelationships  (
+id SERIAL,
 IPversion INT NOT NULL,
 AS1 INT NOT NULL,
 AS2 INT NOT NULL,
@@ -13,6 +14,7 @@ relation INT NULL,
 PRIMARY KEY (IPversion, AS1, AS2, startdate, enddate));
 
 CREATE TABLE IF NOT EXISTS ASRank.CustomerCone(
+id SERIAL,
 IPversion INT NOT NULL,
 AS1 INT NOT NULL,
 Customer INT NOT NULL,
@@ -21,17 +23,20 @@ enddate INT(20) NULL,
 PRIMARY KEY (IPversion, AS1, Customer, startdate, enddate));
 
 CREATE TABLE IF NOT EXISTS ASRank.CustomerConeSize(
+id SERIAL,
 IPversion INT NOT NULL,
 AS1 INT NOT NULL,
 startdate INT NOT NULL,
 Size INT NOT NULL);
 
 CREATE TABLE IF NOT EXISTS ASRank.BGPView(
+id SERIAL,
 IPversion INT NOT NULL,
 startdate INT NOT NULL,
 Size INT NOT NULL);
 
 CREATE TABLE IF NOT EXISTS ASRank.PrefixCone(
+id SERIAL,
 IPversion INT NOT NULL,
 AS1 INT NOT NULL,
 Prefix TEXT NOT NULL,
